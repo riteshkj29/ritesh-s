4c7963d80ac16c22a645ce1d8b273a3663756e52
riteshkj29 <59512312+riteshkj29@users.noreply.github.com>
2020-01-05 03:41:50 +0530

    Add files via upload

---
+++ Assgn5.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int x=3;
+class A {
+    static int x;
+    public:
+        void f1(int x){
+            cout<<"\nValue of static x :"<<A::x;
+            cout<<"\nValue of local x:"<<x;
+            cout<<"\nValue of global x:"<<::x;
+        }
+};
+int A::x = 4;
+int main() {
+    int x=5;
+    A obj;
+    obj.f1(x);
+	return 0;
+}
